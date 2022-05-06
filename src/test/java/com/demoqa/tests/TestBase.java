93a31809f661e16d2b361373e45cca9e10338506
dioliver29 <diana.oliver29@gmail.com>
2022-05-06 23:20:00 +0500

    added value for remote selenide url and base url for jenkins

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -29,14 +29,14 @@ public class TestBase {
     String expectedStateAndCity = format("%s %s", state, city);
 
     public static String remoteSelenideUrl = System.getProperty("remoteSelenideUrl");
-    public static String baseUrl = System.getProperty("baseUrl");
+    //public static String baseUrl = System.getProperty("baseUrl");
 
 
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.baseUrl = baseUrl;
-        Configuration.remote = "https://user1:1234@" + remoteSelenideUrl + "wd/hub";
+        Configuration.baseUrl = System.getProperty("baseUrl");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteSelenideUrl") + "wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
